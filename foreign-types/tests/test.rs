3b3bda8d3d50630bc3cb2d9074e0900fd4d54407
Steven Fackler <sfackler@gmail.com>
2023-03-26 13:27:48 -0400

    fix clippy

--- foreign-types/tests/test.rs
+++ foreign-types/tests/test.rs
@@ -1,10 +1,10 @@
 use foreign_types::foreign_type;
 
 mod foo_sys {
-    pub enum FOO {}
+    pub enum Foo {}
 
-    pub unsafe extern "C" fn foo_drop(_: *mut FOO) {}
-    pub unsafe extern "C" fn foo_clone(ptr: *mut FOO) -> *mut FOO {
+    pub unsafe extern "C" fn foo_drop(_: *mut Foo) {}
+    pub unsafe extern "C" fn foo_clone(ptr: *mut Foo) -> *mut Foo {
         ptr
     }
 
@@ -16,25 +16,25 @@ mod foo_sys {
 
 foreign_type! {
     pub unsafe type Foo<'a, T>: Sync + Send {
-        type CType = foo_sys::FOO;
+        type CType = foo_sys::Foo;
         type PhantomData = &'a T;
         fn drop = foo_sys::foo_drop;
         fn clone = foo_sys::foo_clone;
     }
 
     pub unsafe type FooNoClone {
-        type CType = foo_sys::FOO;
+        type CType = foo_sys::Foo;
         fn drop = foo_sys::foo_drop;
     }
 
     pub unsafe type FooClosure {
-        type CType = foo_sys::FOO;
+        type CType = foo_sys::Foo;
         fn drop = |p| foo_sys::foo_drop(p);
         fn clone = |p| foo_sys::foo_clone(p);
     }
 
     pub unsafe type FooClosureCast {
-        type CType = foo_sys::FOO;
+        type CType = foo_sys::Foo;
         fn drop = |p| foo_sys::foo_drop_requiring_cast(p as _);
         fn clone = |p| foo_sys::foo_clone_requiring_cast(p as _) as _;
     }
